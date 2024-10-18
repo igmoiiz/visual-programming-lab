0e71152c05108eed7b5c4c22972cfc261f4c5370
Moiz <khanmoaiz682@gmail.com>
2024-10-18 20:27:26 +0500

    Design 1 Updated!

--- Lab Tasks/Lab 4/Task 1/obj/Debug/net8.0/VisualProgramming.AssemblyInfo.cs
+++ Lab Tasks/Lab 4/Task 1/obj/Debug/net8.0/VisualProgramming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisualProgramming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cea2abd7a6c026052edd381f479603f8ca7d6d5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb3ffa4516aa4ce6bda94d3319e2bf79bcab7fbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisualProgramming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisualProgramming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
