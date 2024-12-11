59999963c1d58cfb1a89abf370e3f5e71d42afcc
Moiz <khanmoaiz682@gmail.com>
2024-12-11 14:26:40 +0500

    Lab 11 Uploaded!

--- Lab Tasks/Lab 5/Task 1/obj/Debug/net8.0/Task 1.AssemblyInfo.cs
+++ Lab Tasks/Lab 5/Task 1/obj/Debug/net8.0/Task 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9652b2a3e9c94d2b0d96d213c17c27be3778f4bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c3cdfc9616025cd7b02efe3e937054b1bd2fc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
