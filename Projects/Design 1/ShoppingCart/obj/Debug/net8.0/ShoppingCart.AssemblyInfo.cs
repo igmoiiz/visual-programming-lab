9dd703ad9161b393fb2cd311b40a7e3a29086a8e
Moiz <khanmoaiz682@gmail.com>
2024-12-26 22:30:20 +0500

    Ui Updated!

--- Projects/Design 1/ShoppingCart/obj/Debug/net8.0/ShoppingCart.AssemblyInfo.cs
+++ Projects/Design 1/ShoppingCart/obj/Debug/net8.0/ShoppingCart.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7cbca20b37c68c4130b4ac026f38733216be2f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc79645c94af2d1bbfc8371b9683c6cac2cf7d6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
